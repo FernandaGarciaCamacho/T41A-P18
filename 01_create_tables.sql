1c7ba6bf7fc4aa61444a790c59c10a1a32414289
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-18 19:27:58 -0600

    Create 01_create_tables.sql

---
+++ 01_create_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT,
+    etiquetas TEXT[]
+);
+
+CREATE TABLE empleados (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT,
+    jefe_id INT
+);
+
+CREATE TABLE rutas (
+    id SERIAL PRIMARY KEY,
+    ciudad TEXT,
+    destino TEXT
+);
+
